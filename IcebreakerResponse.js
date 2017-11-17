1c8b377b46b7770c54d383c3950724ed81e550be
Sasha V <sashav4@gmail.com>
2017-11-17 05:43:02 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,7 +2,7 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-IcebreakerResponse.BatchCreate = function(emails){
+Icebreaker.BatchCreate = function(emails){
   return emails.map(function(){
     return new IcebreakerResponse(emails)
   })
